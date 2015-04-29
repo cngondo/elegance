06fb87fd0087f6c4bbe5aeb946c2c6302078ce30
Cornellius Ngondo <devngondo@gmail.com>
2015-04-29 11:13:15 +0000

    Add comment

--- app/src/main/java/elegance/example/ngondo/elegance/Home.java
+++ app/src/main/java/elegance/example/ngondo/elegance/Home.java
@@ -12,6 +12,7 @@ public class Home extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //testing git
     }
 
 
