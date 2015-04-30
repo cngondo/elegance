c90e573e31f0b562a56e59b548b240c751240c7f
Cornellius Ngondo <devngondo@gmail.com>
2015-04-30 15:05:27 +0000

    Added splash screen and changed layouts

--- app/src/main/java/elegance/example/ngondo/elegance/Splash.java
+++ app/src/main/java/elegance/example/ngondo/elegance/Splash.java
@@ -1,17 +1,50 @@
 package elegance.example.ngondo.elegance;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class Splash extends ActionBarActivity {
+    private final int splashInterval = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //To make the contents fill the screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                //Starting the splash activity
+                Intent i = new Intent(Splash.this, Appinfo.class);
+                startActivity(i);
+
+
+                //This exits the SplashScreen activity
+                this.finish();
+            }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+
+
     }
 
 
